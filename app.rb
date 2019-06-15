c29846d15f744d51e13f8f55018e0f1ce5eaffc9
Ryan Pethel <arpethel@gmail.com>
2019-06-15 22:37:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,9 +20,9 @@ class App < Sinatra::Base
     # @n = @n.to_i
     # @n.times {puts "hello"}
     # "#{@phrases}"
-  
+
     # "#{@number} #{@phrase}"
-  
+
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
