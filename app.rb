e78fa74adac188152e7181d7243583a11adf9cb1
Ryan Pethel <arpethel@gmail.com>
2019-06-15 22:55:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,11 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
 
-    @n = params[:number].to_i
-    @p = params[:phrase] + "3"
+    # @n = params[:number].to_i
+    # @p = params[:phrase]
+
+    @p = params[:number].to_i + params[:phrase]
+
 
     "#{@p}"
 
